95ab575e683add3dea2356b7cec0876849aef329
Cristiano Henrique Ferreira <13822400661@MUZAMBINHO.EDU.BR>
2026-04-07 22:33:25 -0300

    feat: adicionado protocolo do helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { getEnv } from './shared/utils/env';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
   await app.listen(getEnv('PORT') ?? 3000);
 }
 bootstrap();
